691b7d749b02e1eb85579cf4b8dd9083e6ecdb81
Tommy1012 <973100804@qq.com>
2020-07-29 19:19:13 +0800

    init

---
+++ shop-portal/src/test/java/com/tommy/shop/portal/ShopPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tommy.shop.portal;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
